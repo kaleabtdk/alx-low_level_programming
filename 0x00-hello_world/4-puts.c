65c8d97688e1953e8090ff62e401a0f18036932e
kaleabtdk <kaleabtsedeke@gmail.com>
2021-08-26 13:58:38 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints a sample string
+ * Return: 0 if program executes properly, else a non-zero number
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
